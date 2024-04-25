363e21ef1ac1d15e2d31f11226195924cf04f63d
Otto Oliveira <otto.oliveira@unity3d.com>
2024-04-25 07:13:33 -0700

    Disable APCM

--- source/AndroidProjectGradle/GradleProjectModifier.cs
+++ source/AndroidProjectGradle/GradleProjectModifier.cs
@@ -8,25 +8,25 @@ using UnityEngine;
 namespace Google.Android.ConfigurationManager
 {
     [InitializeOnLoad]
-    public class GradleProjectModifier : AndroidProjectFilesModifier
+    public class GradleProjectModifier// : AndroidProjectFilesModifier
     {
         private static ICollection<Dependency> _dependencies;
 
         static GradleProjectModifier()
         {
-            Debug.Log("GradleProjectModifier Ctor");
-            GradleTemplateResolver.OnResolve -= OnResolve;
-            GradleTemplateResolver.OnResolve += OnResolve;
+            Debug.Log("DISABLED GradleProjectModifier Ctor");
+            // GradleTemplateResolver.OnResolve -= OnResolve;
+            // GradleTemplateResolver.OnResolve += OnResolve;
         }
 
-        public override void OnModifyAndroidProjectFiles(AndroidProjectFiles projectFiles)
+        /*public override void OnModifyAndroidProjectFiles(AndroidProjectFiles projectFiles)
         {
             Debug.Log("OnModifyAndroidProjectFiles");
             Debug.Log($"_dependencies.Count  {_dependencies.Count}");
 
             foreach (var dependency in _dependencies)
                 projectFiles.UnityLibraryBuildGradle.Dependencies.AddDependencyImplementationByName(dependency.Key);
-        }
+        }*/
 
         private static void OnResolve(ICollection<Dependency> obj)
         {
